dfb734dfbb98997025719e0809365efb69d5882e
Tom Wardill <tom@howrandom.net>
2011-08-06 12:42:05 +0100

    Bootstrapped

---
+++ src/LEEDSHACKTHING/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+version = '0.0.1'
+
+setup(
+    name = 'leedshackthing',
+    version = version,
+    description = "",
+    long_description = "",
+    author = "",
+    author_email = "",
+    url = "",
+    license = "",
+    packages = find_packages(),
+    include_package_data = True,
+    zip_safe = False,
+    
+    install_requires = [
+        ]
+    )
