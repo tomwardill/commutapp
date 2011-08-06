1845c78bd7cb92235156a0908e10f813cff6612e
Tom Wardill <tom@howrandom.net>
2011-08-06 13:44:45 +0100

    Added the application

--- src/LEEDSHACKTHING/leedshackthing/settings.py
+++ src/LEEDSHACKTHING/leedshackthing/settings.py
@@ -92,6 +92,8 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
+    'django.contrib.admin',
+    'django.contrib.gis',
     'leedshackthing.main'
 )
 
