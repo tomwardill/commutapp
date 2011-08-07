f68b10fa3041c608e40dfcb5a40dab48af0d9c53
Tom Wardill <tom@howrandom.net>
2011-08-07 09:45:15 +0100

    Better from in the SMS

--- src/LEEDSHACKTHING/leedshackthing/main/views/sms.py
+++ src/LEEDSHACKTHING/leedshackthing/main/views/sms.py
@@ -12,7 +12,7 @@ class SMS(object):
             url = 'http://api.esendex.com/v1.0/messagedispatcher'
             encoded_username = base64.urlsafe_b64encode('sms9@hodgetastic.com:leedshack')
             
-            xml = """<?xml version='1.0' encoding='UTF-8'?><messages><accountreference>%s</accountreference><message><to>%s</to><body>%s</body></message></messages>""" % ('EX0081171', recipient, message)  
+            xml = """<?xml version='1.0' encoding='UTF-8'?><messages><accountreference>%s</accountreference><message><from>Commutr</from><to>%s</to><body>%s</body></message></messages>""" % ('EX0081171', recipient, message)  
             
             request = urllib2.Request(url, data = xml)
             request.headers = {'Content-Type' : 'text/xml', 'Authorization' : "Basic %s" % (encoded_username)}
