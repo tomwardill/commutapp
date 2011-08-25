72db34c7ef3c864ce02022eb26a9223a049f009d
Tom Wardill <tom@howrandom.net>
2011-08-25 15:02:02 +0100

    Fix JS for new fieldnames

--- src/LEEDSHACKTHING/leedshackthing/static/js/commutemap.js
+++ src/LEEDSHACKTHING/leedshackthing/static/js/commutemap.js
@@ -7,7 +7,10 @@ function featureadded(feature) {
     $('#noneToggle')[0].checked = true;
     
     // set the hidden inputs for map boundaries
-    $('#wkt')[0].value = wkt.write(feature.feature);
+    console.log(feature.feature);
+    box_value = wkt.write(feature.feature);
+    console.log(box_value);
+    $('#id_box')[0].value = box_value;
 }
 
 function activate() {
