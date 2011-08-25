4b25ce79bc45df6d8a99c7e10d29a404d44bfcb4
Tom Wardill <tom@howrandom.net>
2011-08-25 12:41:33 +0100

    Commutes now have UI and DB for Choices of day

--- src/LEEDSHACKTHING/leedshackthing/main/forms.py
+++ src/LEEDSHACKTHING/leedshackthing/main/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, HiddenInput, CheckboxSelectMultiple
 
 import models
 
@@ -8,3 +8,13 @@ class CommuteForm(ModelForm):
         
         model = models.Commute
         exclude = ('user')
+        widgets = {
+            'box': HiddenInput(),
+            'day_choices': CheckboxSelectMultiple(),
+        }
+        
+    def __init__(self, *args, **kwargs):
+        super(CommuteForm, self).__init__(*args, **kwargs)
+    
+        # Remove the 'hold ctrl' text, we don't need it, we're using a checkbox select
+        self.fields['day_choices'].help_text = ''
--- src/LEEDSHACKTHING/leedshackthing/main/views/commute.py
+++ src/LEEDSHACKTHING/leedshackthing/main/views/commute.py
@@ -17,7 +17,15 @@ def new(request):
     data = {}
     
     if request.method == 'POST':
-        pass
+        form = CommuteForm(request.POST)
+        if form.is_valid():
+
+            form.instance.user = request.user
+            form.instance.box = fromstr(request.POST['box'])
+            form.save()
+            messages.success(request, 'Commute created.')
+            return HttpResponseRedirect(reverse('index'))
+        data['form'] = form
     else:
         data['form'] = CommuteForm()
     
@@ -28,21 +36,19 @@ def edit(request, commute_id):
     
     
     commute = Commute.objects.get(id = commute_id)
+    form = CommuteForm(instance = commute)
     
     if request.method == 'POST':
 
-        c = commute
-        c.user = request.user
-        c.name = request.POST['name']
-        if request.POST['wkt']:
-            c.box = fromstr(request.POST['wkt'])
-        start_time = date_parse(request.POST['starttime']).time()
-        end_time = date_parse(request.POST['endtime']).time()
-        c.start_time = start_time
-        c.end_time = end_time
+        form = CommuteForm(request.POST, instance = commute)
+        if form.is_valid():
+
+            form.instance.user = request.user
+            form.instance.box = fromstr(request.POST['box'])
+            form.save()
+            messages.success(request, 'Commute edited.')
+            return HttpResponseRedirect(reverse('index'))
         
-        c.save()
-        messages.success(request, 'Commute edited.')
-        return HttpResponseRedirect(reverse('index'))
+
     
     return render_to_response('commute-new.html', locals(), context_instance = RequestContext(request))
